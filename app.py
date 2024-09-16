425905b7b31e0aa47011512c1ed3f3a5821a9d3e
Ben Taylor <bentaylr@gmail.com>
2024-09-16 23:14:51 +1200

    simple landing page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template, session
+from flask import Flask, request, redirect, url_for, render_template
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app.secret_key = 'your_secret_key'  # Change this to a real secret key
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'  # Redirect to login if not authenticated
 
 # Dummy user store
 users = {'user': {'password': 'password'}}
@@ -27,13 +28,13 @@ def login():
         if user and user['password'] == password:
             user = User(username)
             login_user(user)
-            return redirect(url_for('landing'))
+            return redirect(url_for('index'))
     return render_template('login.html')
 
-@app.route('/landing')
+@app.route('/')
 @login_required
-def landing():
-    return render_template('landing.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/logout')
 @login_required
