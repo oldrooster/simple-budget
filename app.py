d14519425ea5fc81f9534cf28cd178695b0d2d22
Ben Taylor <bentaylr@gmail.com>
2024-09-16 23:05:40 +1200

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, redirect, url_for, render_template, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a real secret key
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Dummy user store
+users = {'user': {'password': 'password'}}
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = users.get(username)
+        if user and user['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('landing'))
+    return render_template('login.html')
+
+@app.route('/landing')
+@login_required
+def landing():
+    return render_template('landing.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
